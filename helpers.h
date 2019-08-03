8829f6e2573c7b34fa4449bbe4c21c737ade54a3
ilya-pauzner <ilya-pauzner@yandex.ru>
2019-08-03 18:13:23 +0300

    Algorithms, first draft. Test would be added later.

---
+++ helpers.h
@@ -0,0 +1,10 @@
+#ifndef GROEBNER_HELPERS_H
+#define GROEBNER_HELPERS_H
+
+#include "monomial.h"
+
+namespace Groebner {
+    Monomial lcm(const Monomial&, const Monomial&);
+}
+
+#endif //GROEBNER_HELPERS_H
